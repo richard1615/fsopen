540f519c264e1e50b07663a2d6885c6679a643a2
Malavika Harikumar <malavikahari@gmail.com>
2022-09-27 02:21:15 +0530

    Ex 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -35,16 +35,55 @@ const App = () => {
             </div>
             <Button onClick={castVote} text='vote'/>
             <Button onClick={nextQuote} text='next anecdote'/>
+            <MostVotes scores={scores} anecdotes={anecdotes}/>
         </>
     )
 }
 
-const Button = ({onClick, text}) => {
+const Button = ( {onClick, text} ) => {
     return (
         <button onClick={onClick}>
             {text}
         </button>
     )
 }
+
+const MostVotes = ({ scores, anecdotes }) => {
+    // find index of maximum score
+    function indexOfMax(arr) {
+        if (arr.length === 0) {
+            return -1;
+        }
+
+        let max = arr[0];
+        let maxIndex = 0;
+
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                maxIndex = i;
+                max = arr[i];
+            }
+        }
+
+        return maxIndex;
+    }
+
+    const mostVoted = indexOfMax(scores)
+
+    return (
+        <div>
+            <h1>
+                Anecdote with most votes
+            </h1>
+            <p>
+                {anecdotes[mostVoted]}
+            </p>
+            <p>
+                has {scores[mostVoted]}
+            </p>
+        </div>
+    )
+}
+
 
 export default App
