367cffa55fe0109201cb0268566269843d21a180
Malavika Harikumar <malavikahari@gmail.com>
2022-09-26 23:42:55 +0530

    Ex 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -21,17 +21,35 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState(0)
 
     const handleGood = () => {
         setGood(good + 1)
+        setTotal(total + 1)
     }
 
     const handleBad = () => {
         setBad(bad + 1)
+        setTotal(total + 1)
     }
 
     const handleNeutral = () => {
         setNeutral(neutral + 1)
+        setTotal(total + 1)
+    }
+
+    const calcAvg = () => {
+        if (total == 0) {
+            return 0
+        }
+        return (good - bad) / total //good * 1 + neutral * 0 + bad * -1
+    }
+
+    const calcPos = () => {
+        if (total == 0) {
+            return 0
+        }
+        return good / total
     }
 
     return (
@@ -44,6 +62,12 @@ const App = () => {
             <Data type='good' count={good}/>
             <Data type='neutral' count={neutral}/>
             <Data type='bad' count={bad}/>
+            <p>
+                average {calcAvg()}
+            </p>
+            <p>
+                average {calcPos()} %
+            </p>
         </div>
     )
     }
