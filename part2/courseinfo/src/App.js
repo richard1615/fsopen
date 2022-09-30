aebc9e4a1dc4374f03dc98cae4800d70888c5d0d
Malavika Harikumar <malavikahari@gmail.com>
2022-09-30 11:35:40 +0530

    Ex 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,29 +1,5 @@
-const Header = ({ course }) => <h2>{course}</h2>
+import Course from "./components/Course";
 
-const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
-
-
-const Part = ({ part }) =>
-    <p>
-      {part.name} {part.exercises}
-    </p>
-
-const Content = ({ parts }) =>
-    <>
-        {parts.map((part) => <Part key={part.id} part={part}/>)}
-    </>
-
-const Course = ({ course }) => {
-    const total = course.parts.reduce((sum, part) => sum + part.exercises,0)
-
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total sum={total}/>
-        </div>
-    )
-}
 
 const App = () => {
     const courses = [
