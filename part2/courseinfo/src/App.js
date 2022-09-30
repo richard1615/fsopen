04f5076903c4fea42d12185987e1e6781ced415e
Malavika Harikumar <malavikahari@gmail.com>
2022-09-30 11:23:16 +0530

    Ex 2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-// const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+//const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
 const Part = ({ part }) =>
     <p>
