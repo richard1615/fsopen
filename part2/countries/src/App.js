97a3d3bfeffe2dafa70f8d69aab72733cdea69d5
Malavika Harikumar <malavikahari@gmail.com>
2022-10-13 10:17:11 +0530

    Ex 2.12 got data

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,24 @@
-function App() {
-  return (
-    <div className="App">
+import axios from "axios"
+import { useState, useEffect } from "react";
 
+function App() {
+  const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+        .get("https://restcountries.com/v3.1/all")
+        .then(response => {
+          setCountries(response.data)
+          console.log(response.data)
+        })
+  }, [])
+
+  return (
+    <div>
+        <ul>
+            {countries.map((c) => <li key={c.name.common}>{c.name.common}</li>)}
+        </ul>
     </div>
   );
 }
