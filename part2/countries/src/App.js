0c83fc8fb027d6ed1e449e0212d899eddf1055d0
Malavika Harikumar <malavikahari@gmail.com>
2022-10-13 10:46:02 +0530

    Ex 2.12 search function

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,26 +1,34 @@
 import axios from "axios"
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 function App() {
-  const [search, setSearch] = useState('')
-  const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState('')
+    const [countries, setCountries] = useState([])
+    const [filtered, setFiltered] = useState([])
 
-  useEffect(() => {
-    axios
-        .get("https://restcountries.com/v3.1/all")
-        .then(response => {
-          setCountries(response.data)
-          console.log(response.data)
-        })
-  }, [])
+    useEffect(() => {
+        axios
+            .get("https://restcountries.com/v3.1/all")
+            .then(response => {
+                setCountries(response.data)
+                console.log(response.data)
+            })
+    }, [])
 
-  return (
-    <div>
-        <ul>
-            {countries.map((c) => <li key={c.name.common}>{c.name.common}</li>)}
-        </ul>
-    </div>
-  );
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value)
+        const newFiltered = countries.filter((c) => c.name.common.toLowerCase().includes(search.toLowerCase()))
+        setFiltered(newFiltered)
+    }
+
+    return (
+        <div>
+            find countries <input type="text" value={search} onChange={handleSearchChange}/>
+            <ul>
+                {filtered.map((c) => <li key={c.name.common}>{c.name.common}</li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
