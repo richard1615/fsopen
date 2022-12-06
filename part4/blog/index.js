41f313d9363df18c1c7f8bf819305794c4ffcfc2
Malavika <malavikahari02@gmail.com>
2022-12-06 16:49:46 +0530

    added middleware module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -6,29 +6,13 @@ const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controller/notes')
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
-const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-}
+const middleware = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
