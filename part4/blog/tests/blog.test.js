e38cc2ca481f8d10e903d94087f903a5deb36c50
Malavika <malavikahari02@gmail.com>
2022-12-07 15:23:07 +0530

    Ex. 4.5

--- part4/blog/tests/blog.test.js
+++ part4/blog/tests/blog.test.js
@@ -24,3 +24,33 @@ describe('total likes', () => {
     })
 })
 
+describe('favourite blog', () => {
+    test('of empty list is None', () => {
+        const result = listHelper.favouriteBlog([])
+        expect(result).toEqual(NaN)
+    })
+
+    test('when list has only one blog, equals the blog itself', () => {
+        const result = listHelper.favouriteBlog(data.listWithOneBlog)
+        expect(result).toEqual({
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        })
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.favouriteBlog(data.listWithManyBlogs)
+        expect(result).toEqual({
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        })
+    })
+})
