a9e1e09ba1d17a0b373e99d4566c3d35ecb9d793
Malavika <malavikahari02@gmail.com>
2022-12-24 11:56:55 +0530

    Ex 4.15

---
+++ part4/blog/controller/users.js
@@ -0,0 +1,21 @@
+const User = require('../models/user')
+const userRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+
+userRouter.post('/',async (request, response) => {
+    const { username, name, password } = request.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+module.exports = userRouter
