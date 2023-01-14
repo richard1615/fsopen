fc58d5eff55a9130d737614f5a1a103c8a332703
Malavika <malavikahari02@gmail.com>
2023-01-14 10:47:49 +0530

    Ex. 5.4

--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,11 @@
 import {useState} from "react"
 import blogService from '../services/blogs'
 
-const Blog = ({blogs, setBlogs, setMessage}) => {
+const BlogForm = ({ setMessage, setBlogs, blogs }) => {
   const [ title, setTitle ] = useState('')
   const [ author, setAuthor ] = useState('')
   const [ url, setUrl ] = useState('')
+
   const handleAddPost = (e) => {
     e.preventDefault()
     const newBlogObject = {
@@ -32,35 +33,37 @@ const Blog = ({blogs, setBlogs, setMessage}) => {
       setMessage(null)
     }, 5000)
   }
-  const blogForm = () => {
-    return (
-      <form action="" method="post" onSubmit={handleAddPost}>
-        <div>
-          title
-          <input type='text' name='title' value={title}
-                 onChange={({target}) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author
-          <input type='text' name='author' value={author}
-                 onChange={({target}) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url
-          <input type='text' name='url' value={url}
-                 onChange={({target}) => setUrl(target.value)}
-          />
-        </div>
-        <input type='submit' value='Add blog'/>
-      </form>
-    )
-  }
+
+  return (
+    <form action="" method="post" onSubmit={handleAddPost}>
+      <div>
+        title
+        <input type='text' name='title' value={title}
+               onChange={({target}) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input type='text' name='author' value={author}
+               onChange={({target}) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input type='text' name='url' value={url}
+               onChange={({target}) => setUrl(target.value)}
+        />
+      </div>
+      <input type='submit' value='Add blog'/>
+    </form>
+  )
+}
+
+const Blog = ({blogs, setBlogs, setMessage}) => {
   return (
     <>
       <h2>blogs</h2>
-      {blogForm()}
+      <BlogForm blogs={blogs} setBlogs={setBlogs} setMessage={setMessage}/>
       <ul>
         {blogs.map(blog =>
           <li key={blog.id}>{blog.title} {blog.author}</li>
