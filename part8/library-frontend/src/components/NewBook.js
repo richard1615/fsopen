8add3d396598582f3abe944206773488846e89e0
Malavika Harikumar <malavikahari02@gmail.com>
2023-04-05 13:11:21 +0530

    add books

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK, GET_ALL_BOOKS, GET_AlL_AUTHORS } from "../queries";
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,6 +8,10 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [ addBook ] = useMutation(ADD_BOOK, {
+    refetchQueries: [ { query: GET_ALL_BOOKS }, { query: GET_AlL_AUTHORS } ],
+  })
 
   if (!props.show) {
     return null
@@ -15,6 +21,8 @@ const NewBook = (props) => {
     event.preventDefault()
 
     console.log('add book...')
+
+    addBook({ variables: { title, author, published: Number(published), genres } })
 
     setTitle('')
     setPublished('')
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -19,3 +19,19 @@ export const GET_ALL_BOOKS = gql`
     }
   }
 `
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      genres
+    }
+  }
+`
